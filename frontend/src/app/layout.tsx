23dedbee4ff555f666abd8a704154f8d4ae18c5d
aiden-leee <wlvmf3409@naver.com>
2025-11-06 07:11:54 +0900

    [feat] clerk 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <TanstackProvider>
       <ClerkProvider
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
         appearance={{
           variables: {
             colorPrimary: "#1e9df1",
