5a100338dd3f2c9ce74e0290db0938ca97bc5d3d
aiden-leee <wlvmf3409@naver.com>
2025-11-07 00:57:27 +0900

    [feat] next.config 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -26,6 +26,15 @@ const nextConfig: NextConfig = {
     // 이미지 캐시 시간 증가 (초 단위)
     minimumCacheTTL: 60,
   },
+  // env: {
+  //   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  //   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  //   NEXT_PUBLIC_VAPI_ASSISTANT_ID: process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID,
+  //   NEXT_PUBLIC_VAPI_API_KEY: process.env.NEXT_PUBLIC_VAPI_API_KEY,
+  //   NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  //   NEXT_PUBLIC_APP_URL_CLIENT: process.env.NEXT_PUBLIC_APP_URL_CLIENT,
+  //   NEXT_PUBLIC_ADMIN_EMAIL: process.env.NEXT_PUBLIC_ADMIN_EMAIL,
+  // }
 };
 
 export default nextConfig;
